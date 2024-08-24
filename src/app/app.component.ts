fedd95a5ab94b9819ab1e630688c3bd058475cbc
Peter Carter <peter.carter.mail@gmail.com>
2024-08-24 16:59:44 +0100

    add tutorial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { NavigationStart, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ObservingtheObservables';
+  routes = routes as [{ path: string; data: { page: number } }];
+  router = inject(Router);
+  changeDetectorRef = inject(ChangeDetectorRef);
+  isRoot = false;
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isRoot = event.url === '/';
+        this.changeDetectorRef.detectChanges();
+      }
+    });
+    }
 }
