f23b7e980a653fa8f6d5fd8d4d42b32b745590ae
Peter Carter <peter.carter.mail@gmail.com>
2024-08-25 10:50:55 +0100

    add base component

---
+++ src/app/errors-in-the-observable-2/errors-in-the-observable-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-errors-in-the-observable-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './errors-in-the-observable-2.component.html',
+  styleUrl: './errors-in-the-observable-2.component.scss'
+})
+export class ErrorsInTheObservable2Component {
+
+}
