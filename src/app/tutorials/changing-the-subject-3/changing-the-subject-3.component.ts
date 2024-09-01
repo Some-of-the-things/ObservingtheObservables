a9048ab7f5d00fcee03d8ef057240236fde91a67
Some-of-the-things <peter.carter.mail@gmail.com>
2024-09-01 13:26:08 +0100

    add changing the subject tutorial

--- src/app/tutorials/changing-the-subject-3/changing-the-subject-3.component.ts
+++ src/app/tutorials/changing-the-subject-3/changing-the-subject-3.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Subject, BehaviorSubject, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-changing-the-subject-3',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './changing-the-subject-3.component.html',
   styleUrl: './changing-the-subject-3.component.scss'
 })
-export class ChangingTheSubject2Component {
-  
+export class ChangingTheSubject3Component implements OnInit {
+  subject$ = new Subject<string>();
+  behaviorSubject$ = new BehaviorSubject<string>('Your behavior');
+  subjectSubscription$ = new Subscription();
+  behaviorSubscription$ = new Subscription();
+
+  subjectChange: string = '';
+  theSubject: string | null = null;
+  behavior = false;
+
+  ngOnInit() {
+    this.#subjectSubscribe();
+  }
+
+  changeSubject() {
+    if (this.behavior) {
+      this.behaviorSubject$.next(this.subjectChange);
+    } else {
+      this.subject$.next(this.subjectChange);
+    }
+  }
+
+  forgetSubject() {
+    this.theSubject = null;
+    this.behaviorSubject$ = new BehaviorSubject('Your behavior');
+    this.subject$ = new Subject();
+    if (this.behavior) {
+      this.#behaviourSubjectSubscribe();
+    } else {
+      this.#subjectSubscribe();
+    }
+  }
+
+  changeBehavior() {
+    if (!this.behavior) {
+      this.subjectSubscription$.unsubscribe();
+      this.behaviorSubscription$ = this.behaviorSubject$.subscribe((sub) => {
+        this.theSubject = sub;
+      });
+    } else {
+      this.#subjectSubscribe();
+      this.behaviorSubscription$.unsubscribe();
+    }
+    this.behavior = !this.behavior;
+  }
+
+  #subjectSubscribe() {
+    this.subjectSubscription$ = this.subject$.subscribe((sub) => {
+      this.theSubject = sub;
+    });
+  }
+
+  #behaviourSubjectSubscribe() {
+    this.behaviorSubscription$ = this.behaviorSubject$.subscribe((sub) => {
+      this.theSubject = sub;
+    });
+  }
 }
