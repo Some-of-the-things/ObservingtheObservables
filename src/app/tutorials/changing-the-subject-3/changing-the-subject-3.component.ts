a09ba937c5a33840fff28bfc96568e5304483226
Some-of-the-things <peter.carter.mail@gmail.com>
2024-09-01 11:35:52 +0100

    base components

---
+++ src/app/tutorials/changing-the-subject-3/changing-the-subject-3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changing-the-subject-3',
+  standalone: true,
+  imports: [],
+  templateUrl: './changing-the-subject-3.component.html',
+  styleUrl: './changing-the-subject-3.component.scss'
+})
+export class ChangingTheSubject2Component {
+  
+}
